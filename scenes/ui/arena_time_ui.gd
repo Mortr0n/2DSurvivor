6e26b81d3cb2a5e71bd525885ae4db9712793dea
Mortron <cmsl4y3r@gmail.com>
2024-01-14 18:34:04 -0700

    hurtbox/hitbox damage to enemies set up

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -1,3 +1,18 @@
 extends CanvasLayer
 
 @export var arena_time_manager: Node
+@onready var label = %Label
+
+func _process(delta):
+	if arena_time_manager == null:
+		return
+	var time_elapsed = arena_time_manager.get_time_elapsed()
+	var time_elapsed_string_for_label = format_seconds_to_string(time_elapsed)
+	label.text = str(time_elapsed_string_for_label)
+	
+	
+func format_seconds_to_string(seconds: float):
+	var minutes = floor(seconds / 60)
+	var remaining_seconds = seconds - (minutes * 60)
+	#print("seconds: " + str(seconds) + "minutes: " + str(minutes) + "minutes*60: " + str(minutes * 60))
+	return str(minutes) + ":" + "%02d" % floor(remaining_seconds)
