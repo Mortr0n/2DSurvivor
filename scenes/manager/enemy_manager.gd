d99a6b0e4c6d7369b3a94c374bfa222043638b9d
Mortron <cmsl4y3r@gmail.com>
2024-01-20 10:15:04 -0700

    set to not spawn enemies behind collidable objects

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -11,11 +11,35 @@ const SPAWN_RADIUS = 375
 
 var base_spawn_time = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
+
+
+func get_spawn_position(timesRun: int): # when I take over look to pass in the player: CharacterBody2D Not sure I want to look for it twice.
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return Vector2.ZERO
+	
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		
+	# we are checking if there would be a wall collision between the enemy and the player using ray tracing.
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var collision_result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+		
+		if collision_result.size() == 0:
+			return spawn_position
+		else:
+			random_direction = random_direction.rotated(1.5708) # It's in radians.  I just converted 90 deg, but there is a deg_to_rad, but is it worth the extra calc???
+	
+	if timesRun < 4:
+		return get_spawn_position(timesRun + 1)
+	return Vector2.ZERO
 
 
 func on_timer_timeout():
@@ -25,17 +49,13 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
-	
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 #	could look if the layer exists in case of the layer not being there for some reason
 	entities_layer.add_child(enemy)
 	#get_parent().add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position(0)
 
 func on_arena_difficulty_increased(arena_difficulty_level: int):
 	var max_time_off = .5
@@ -44,7 +64,7 @@ func on_arena_difficulty_increased(arena_difficulty_level: int):
 	#print(time_off)
 	# how tutorial did the time off above
 	var time_off = arena_difficulty_level * arena_difficulty_time_decrease
-	print(time_off)
+	#print(time_off)
 	#time_off = max(time_off, .3) this was his as well.  Not sure about this one
 	timer.wait_time = max((base_spawn_time - time_off), .5)
-	print(timer.wait_time)
+	#print(timer.wait_time)
